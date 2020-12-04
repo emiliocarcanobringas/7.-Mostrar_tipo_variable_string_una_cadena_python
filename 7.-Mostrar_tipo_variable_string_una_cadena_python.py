6ab2b09f1b22c42a7e380198360dc58759b1a919
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 19:52:25 -0600

    Add files via upload

---
+++ 7.-Mostrar_tipo_variable_string_una_cadena_python.py
@@ -0,0 +1,18 @@
+# Este programa muestra en pantalla el valor de una variable de tipo string
+
+print("Este programa muestra en pantalla el valor de una variable de tipo string")
+print("La mostramos con comillas simples y comillas dobles")
+print("También mostramos el tipo de la variable")
+cadena = "Agla, On, Matón"
+cadena2 = 'Mi luz brilla más ahora con los poderes de Agla, On, Matón'
+print("El valor de la cadena es: ")
+print(cadena)
+print("El tipo de valor de la cadena es:")
+print(type(cadena))
+print("El valor de la cadena 2 es: ")
+print(cadena2)
+print("El tipo de valor de la cadena es:")
+print(type(cadena2))
+print("Este programa fue escrito por Emilio Carcaño Bringas")
+
+#Este programa fue escrito por Emilio Carcaño Bringas
